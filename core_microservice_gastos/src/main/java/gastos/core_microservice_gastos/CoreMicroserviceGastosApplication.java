bd765de6d465e7f4fbeb8fb6f9525bd35aa84bb9
cesargarridor <cesargarrido2004@gmail.com>
2024-11-04 14:04:44 +0100

    feat(Microservicio Gastos):Proyecto base de gastos creado

---
+++ core_microservice_gastos/src/main/java/gastos/core_microservice_gastos/CoreMicroserviceGastosApplication.java
@@ -0,0 +1,13 @@
+package gastos.core_microservice_gastos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreMicroserviceGastosApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoreMicroserviceGastosApplication.class, args);
+    }
+
+}
